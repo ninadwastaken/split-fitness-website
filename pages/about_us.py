f35d40b7edadcb513b69f28b10ad106c33c3af17
ninad <ninad2eyes@gmail.com>
2024-04-17 01:35:19 -0400

    added about me links and names

--- pages/about_us.py
+++ pages/about_us.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.header("We are Split Fitness.")
+st.title("We are Split Fitness.")
+
+
+c1, _, c2, _, c3 = st.columns(5)
+
+with c1:
+    st.header("Karan Pandey")
+    st.link_button(url="https://www.linkedin.com/in/karan-pandey-079308252/", label="linkedin")
+
+with c2:
+    st.header("Ninad Moharir")
+    st.link_button(url="https://www.linkedin.com/in/ninad-moharir", label="linkedin")
+
+with c3:
+    st.header("Pranav Penmatcha")
+    st.link_button(url="https://www.linkedin.com/in/pranav-penmatcha/", label="linkedin")
